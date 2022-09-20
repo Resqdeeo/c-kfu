b70be2455844cdee286ac51aca0463583ee94f88
Resqdeeo <vlazer03@mail.ru>
2022-09-20 13:39:28 +0300

    5th save

--- Resqd/Resqd/Program.cs
+++ Resqd/Resqd/Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("BB");
+
+/*
+double x;
+x = double.Parse(Console.ReadLine());
+
+double y = 0;
+double p = 1;
+
+int i = 10;
+
+while (i > 0)
+{
+    y = y + i * p;
+    p = p * x;
+    i--;
+}
+Console.WriteLine(y);
+*/
+
+/*
+double x;
+x = double.Parse(Console.ReadLine());
+
+double y = 0;
+double p = x;
+int i = 10;
+
+while (i > 0)
+{
+    y = y + Math.Cos(x);
+    p = p * x;
+    i--;
+}
+Console.WriteLine(y);
+*/
+
+
+
