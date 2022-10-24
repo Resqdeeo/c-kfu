a02676a125dcc1ab4db9c5dc1248556cd48ab869
Resqdeeo <vlazer03@mail.ru>
2022-10-24 15:19:34 +0300

    methods and reads

--- Resqd/Resqd/Program.cs
+++ Resqd/Resqd/Program.cs
@@ -1,69 +1,119 @@
-﻿/*String str = "kabak omar uk ukrku";
-bool fl = true;
-
-foreach (var word in str.Split(' '))
+﻿/*
+void Hello(string s)
 {
-    fl = true;
-    for (int i = 0; i < word.Length / 2; i++)
-    {
-        if (word[i] != word[word.Length - 1 - i])
-        {
-            fl = false;
-        }
-    }
-    if (fl)
-    {
-        Console.WriteLine(word);
-    }
+    Console.WriteLine(s);
 }
+
+int Sum(int x, int y)
+{
+    return x + y;
+    x = 6; y = 7;            //Не будет выполняться
+}
+
+void Swap(ref int x, ref int y)
+{
+    var t = x;
+    x = y;
+    y = t;
+}
+
+int ArrSum(int[] mas)
+{
+    int s = 0;
+    for(int i = 0; i < mas.Length; i++)
+    {
+        s += mas[i];
+    }
+    return s;
+}
+
+void GetInt(out int k)
+{
+    Random rnd = new Random();
+    k = rnd.Next(100);
+}
+
+
+int a = 5, b = 6;
+
+Swap(ref a, ref b);
+Console.WriteLine($"a = {a} b = {b}");
+
+Console.WriteLine(Sum(a, b));
+
+int m = 10;
+
+GetInt(out m);
+
+Console.WriteLine($"m = {m}");
+
+
+Hello("Hello World!");
+Hello("Hello 2!");
+Hello("Some text");
+
+string s = "Text 1";
+
+Hello(s);
+
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine(ArrSum(arr));
+
+foreach (int k in arr)
+    Console.WriteLine(k);
 */
 
 
-
-/*
-String str = "kabak omaor uk ukraine";
-bool flaggie = false;
-
-foreach (var biggie in str.Split(' '))
+using StreamWriter writer = new StreamWriter(@"D:\Temp\output.txt", true);
 {
-    flaggie = false;
-    for (int i = 1; i < biggie.Length; i++)
+    char ch = '\0';
+    writer.WriteLine("My Text!");
+    writer.Close();
+}
+
+//using StreamReader reader = new StreamReader(@"D:\Temp\input.txt");
+//{
+//    string str;
+
+//    //while (!reader.EndOfStream)
+//    //{
+//    //    str = reader.ReadLine();
+//    //    Console.WriteLine(str);
+//    //}
+
+//    str = reader.ReadToEnd();
+//    Console.WriteLine(str);
+
+//    int k = 1;
+//    foreach (string c in str.Split('\n'))
+//        Console.WriteLine($"{k++} - {c}");
+//}
+
+int[,] matr;
+
+using StreamReader reader = new StreamReader(@"D:\Temp\InMatr.txt");
+{
+    var mn = reader.ReadLine().Split(' ');
+    int m = int.Parse(mn[0]);
+    int n = int.Parse(mn[1]);
+
+    matr = new int[m, n];
+
+    for(int i = 0; i < m; i++)
     {
-        if (biggie[i] == biggie[0])
+        var row = reader.ReadLine().Split(' ');
+        for(int j = 0; j < n; j++)
         {
-            flaggie = true;
+            matr[i, j] = int.Parse(row[j]);
         }
-    }
-    if (flaggie)
-    {
-        Console.WriteLine(biggie);
     }
 }
-*/
 
 
-/*
-String str = "abcd omaor uk ukraine";
-bool flaggie = false;
-
-foreach (var biggie in str.Split(' '))
+for (int i = 0; i < matr.GetLength(0); i++)
 {
-    flaggie = false;
-    for (int i = 1; i < biggie.Length; i++)
-    {
-        if (biggie[i] >= biggie[i - 1])
-        {
-            flaggie = true;
-        }
-        else
-        {
-            flaggie = false;
-            break;
-        }
-    }
-    if (flaggie)
-    {
-        Console.WriteLine(biggie);
-    }
+    for (int j = 0; j < matr.GetLength(1); j++)
+        Console.Write($"{matr[i, j], 3}");
+    Console.WriteLine();
 }
-*/
