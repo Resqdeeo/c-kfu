5ff25e7925e47c749ea2c8aace6ef1971d1fb325
Resqdeeo <vlazer03@mail.ru>
2022-10-25 13:37:25 +0300

    countwords

--- Resqd/Resqd/Program.cs
+++ Resqd/Resqd/Program.cs
@@ -1,119 +1,83 @@
-﻿/*
-void Hello(string s)
-{
-    Console.WriteLine(s);
-}
-
-int Sum(int x, int y)
-{
-    return x + y;
-    x = 6; y = 7;            //Не будет выполняться
-}
-
-void Swap(ref int x, ref int y)
-{
-    var t = x;
-    x = y;
-    y = t;
-}
-
-int ArrSum(int[] mas)
+﻿/*int ArrSum(ref int[] mas)
 {
     int s = 0;
-    for(int i = 0; i < mas.Length; i++)
+    for (int i = 0; i < mas.Length; i++)
     {
         s += mas[i];
     }
+    mas = new int[5] { 1, 1, 1, 1, 1 };
     return s;
 }
 
-void GetInt(out int k)
-{
-    Random rnd = new Random();
-    k = rnd.Next(100);
-}
-
-
-int a = 5, b = 6;
-
-Swap(ref a, ref b);
-Console.WriteLine($"a = {a} b = {b}");
-
-Console.WriteLine(Sum(a, b));
-
-int m = 10;
-
-GetInt(out m);
-
-Console.WriteLine($"m = {m}");
-
-
-Hello("Hello World!");
-Hello("Hello 2!");
-Hello("Some text");
-
-string s = "Text 1";
-
-Hello(s);
-
 
 int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-Console.WriteLine(ArrSum(arr));
+Console.WriteLine(ArrSum(ref arr));
+
 
 foreach (int k in arr)
-    Console.WriteLine(k);
+{
+    Console.WriteLine(k);                      
+}
 */
 
+/*
+List<char> lst = new List<char>();
+Dictionary<char, int> symbols = new Dictionary<char, int>();
 
-using StreamWriter writer = new StreamWriter(@"D:\Temp\output.txt", true);
+
+using (StreamReader reader = new StreamReader(@"D:\Temp\text.txt"))
 {
-    char ch = '\0';
-    writer.WriteLine("My Text!");
-    writer.Close();
-}
-
-//using StreamReader reader = new StreamReader(@"D:\Temp\input.txt");
-//{
-//    string str;
-
-//    //while (!reader.EndOfStream)
-//    //{
-//    //    str = reader.ReadLine();
-//    //    Console.WriteLine(str);
-//    //}
-
-//    str = reader.ReadToEnd();
-//    Console.WriteLine(str);
-
-//    int k = 1;
-//    foreach (string c in str.Split('\n'))
-//        Console.WriteLine($"{k++} - {c}");
-//}
-
-int[,] matr;
-
-using StreamReader reader = new StreamReader(@"D:\Temp\InMatr.txt");
-{
-    var mn = reader.ReadLine().Split(' ');
-    int m = int.Parse(mn[0]);
-    int n = int.Parse(mn[1]);
-
-    matr = new int[m, n];
-
-    for(int i = 0; i < m; i++)
+    var str = reader.ReadToEnd();
+    foreach(char ch in str)
     {
-        var row = reader.ReadLine().Split(' ');
-        for(int j = 0; j < n; j++)
+        if(Char.IsLetter(ch))
         {
-            matr[i, j] = int.Parse(row[j]);
+            if (!lst.Contains(ch))
+            {
+                lst.Add(ch);
+            }
+            if(!symbols.Keys.Contains(ch))
+            {
+                symbols.Add(ch, 1);
+            }
+            else
+            {
+                int k = symbols[ch];
+                symbols[ch] += 1;
+            }
         }
     }
 }
 
-
-for (int i = 0; i < matr.GetLength(0); i++)
+foreach(var ch in lst)
 {
-    for (int j = 0; j < matr.GetLength(1); j++)
-        Console.Write($"{matr[i, j], 3}");
-    Console.WriteLine();
+    Console.Write(ch);
 }
+
+foreach(char ch in symbols.Keys)
+{
+    Console.WriteLine($"{ch} - {symbols[ch]}");
+}
+
+
+foreach(var k in symbols.Values)
+{
+    Console.WriteLine(k);
+}
+*/
+
+
+
+
+
+using (StreamReader reader = new StreamReader(@"D:\Temp\wordscnt.txt"))
+{
+    string words = await reader.ReadToEndAsync();
+    string[] count = words.Split(new char[] { ' ', '?', '\n', ',', '.', '\t', '\r'  },StringSplitOptions.RemoveEmptyEntries);
+    foreach(var item in count)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+Console.WriteLine($"Количество слов - {countwords}");
